f4accc203df479ddc0c4a6b582fb25e16169cab7
Steven Zhu <stevenzhu03@gmail.com>
2020-01-02 23:00:41 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,23 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  result ={}
+  
+  data.each do |color_gender_lives, value|
+    value.each do |stat, names|
+      names.each do |name|
+        
+        if !result[name]
+          result[name] = {}
+        end
+        
+        if !result[name][color_gender_lives]
+          result[name][color_gender_lives] = []
+        end
+    
+    result[name][color_gender_lives] << stat.to_s
+    
+      end
+    end
+  end
+  
+  result
 end
