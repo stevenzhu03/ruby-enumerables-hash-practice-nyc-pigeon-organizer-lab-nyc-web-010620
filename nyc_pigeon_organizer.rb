0b495307217507e30535ae8bd127645f8bee3bdc
Steven Zhu <stevenzhu03@gmail.com>
2020-01-02 22:39:00 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,8 +1,8 @@
 def nyc_pigeon_organizer(data)
   result ={}
   
-  data.each do |information, value|
-    value.each do |
+  data.each do |color_gender_lives, value|
+    color_gender_lives.each do |
     
   
 end
